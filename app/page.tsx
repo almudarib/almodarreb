55e535a2403a1f72309c020b32a7b7dc3ffb5f71
Montaser0 <montaserhajomar8@gmail.com>
2026-01-24 16:37:28 +0300

    تغير صفحة الroot

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
+import logo from "@/app/public/logo.jpeg";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex items-center justify-center mb-4">
+          <Image src={logo} alt="Logo" width={200} height={200} className="rounded-lg" />
+        </div>
+        <LoginForm />
+      </div>
     </div>
   );
 }
